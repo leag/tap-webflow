ddc5a29539a65e716f15962987f66b09fbb5444e
Luis <luisatala@preyhq.com>
2025-03-12 11:30:37 -0300

    Initial commit

---
+++ tap_webflow/tap.py
@@ -0,0 +1,69 @@
+"""Webflow tap class."""
+
+from __future__ import annotations
+
+from singer_sdk import Tap
+from singer_sdk import typing as th  # JSON schema typing helpers
+
+# TODO: Import your custom stream types here:
+from tap_webflow import streams
+
+
+class TapWebflow(Tap):
+    """Webflow tap class."""
+
+    name = "tap-webflow"
+
+    # TODO: Update this section with the actual config values you expect:
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "auth_token",
+            th.StringType,
+            required=True,
+            secret=True,  # Flag config as protected.
+            title="Auth Token",
+            description="The token to authenticate against the API service",
+        ),
+        th.Property(
+            "project_ids",
+            th.ArrayType(th.StringType),
+            required=True,
+            title="Project IDs",
+            description="Project IDs to replicate",
+        ),
+        th.Property(
+            "start_date",
+            th.DateTimeType,
+            description="The earliest record date to sync",
+        ),
+        th.Property(
+            "api_url",
+            th.StringType,
+            title="API URL",
+            default="https://api.mysample.com",
+            description="The url for the API service",
+        ),
+        th.Property(
+            "user_agent",
+            th.StringType,
+            description=(
+                "A custom User-Agent header to send with each request. Default is "
+                "'<tap_name>/<tap_version>'"
+            ),
+        ),
+    ).to_dict()
+
+    def discover_streams(self) -> list[streams.WebflowStream]:
+        """Return a list of discovered streams.
+
+        Returns:
+            A list of discovered streams.
+        """
+        return [
+            streams.GroupsStream(self),
+            streams.UsersStream(self),
+        ]
+
+
+if __name__ == "__main__":
+    TapWebflow.cli()
